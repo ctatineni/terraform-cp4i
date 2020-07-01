d05b887ea29baeca3e19eae8d27a95c752780d9f
chandhubabu <chandhut@gmail.com>
2020-06-30 20:42:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ module "cp4i" {
   helper               = var.helper
   helper_public_ip     = var.helper_public_ip
   ssh_private_key      = var.ssh_private_key
+  key                  = var.key
 }
 
